dd65abd0e15aa5479d253c4fc5329f5f224acead
andreashgk <andreas@hgk.me>
2024-10-20 21:57:16 +0200

    Allow pill buttons to be expandable

--- ags/quicksettings/quicksettings.ts
+++ ags/quicksettings/quicksettings.ts
@@ -1,5 +1,6 @@
 import Gtk from "gi://Gtk?version=3.0";
 import { Icon } from "lib/types";
+import { FlowBoxChild } from "types/@girs/gtk-3.0/gtk-3.0.cjs";
 import { Binding } from "types/service";
 
 const audio = await Service.import("audio");
@@ -71,6 +72,7 @@ const PillButton = (props: {
   subtext?: Binding<any, any, string | null>,
   active: boolean | Binding<any, any, boolean>,
   onToggled: (event: { active: boolean }) => void,
+  onSubmenu?: () => void,
 }) => {
   const main_label = Widget.Label({
     hpack: "start",
@@ -96,177 +98,205 @@ const PillButton = (props: {
     return children;
   });
 
-  return Widget.ToggleButton({
-    className: "pill-button",
+  return Widget.Box({
+    setup(self) {
+      if (props.onSubmenu !== undefined) {
+        self.add(
+          Widget.Button({
+            className: "chevron",
+            child: Widget.Icon({
+              size: 15,
+              icon: "go-next-symbolic",
+            }),
+            onClicked() {
+              // Typescript is unhappy without the cast.
+              (props as any).onSubmenu();
+            },
+          }),
+        );
+      }
+    },
     hexpand: false,
-    active: props.active,
-    onToggled: props.onToggled,
+    children: [
+      Widget.ToggleButton({
+        className: "pill-button",
+        hexpand: true,
+        active: props.active,
+        onToggled: props.onToggled,
+        child: Widget.Box({
+          vertical: false,
+          children: [
+            Widget.Box({
+              hexpand: true,
+              children: [
+                Widget.Icon({
+                  size: 18,
+                  icon: props.icon,
+                }),
+                Widget.Box({
+                  vertical: true,
+                  vexpand: true,
+                  vpack: "center",
+                  children: labels,
+                }),
+              ],
+            }),
+          ],
+        }),
+      }),
+    ],
+  })
+};
+
+export const Quicksettings = () => {
+  let top_button_battery = Button({
+    icon: battery.bind("icon_name"),
+    label: battery.bind("percent").as(percent => `  ${percent}%`),
+    on_click() {
+      Utils.execAsync(`bash -c "XDG_CURRENT_DESKTOP=gnome gnome-control-center power"`);
+    },
+  });
+
+  let top_buttons = [
+    Button({
+      icon: "applets-screenshooter-symbolic",
+    }),
+    Button({
+      icon: "settings-symbolic",
+      on_click() {
+        Utils.execAsync(`bash -c "XDG_CURRENT_DESKTOP=gnome gnome-control-center"`);
+      },
+    }),
+    Button({
+      icon: "system-lock-screen-symbolic",
+    }),
+    Button({
+      icon: "system-shutdown-symbolic",
+    }),
+  ];
+
+  return Widget.Window({
+    visible: false,
+    name: "quicksettings",
+    anchor: ['top', 'right'],
+    layer: "top",
+    exclusivity: "exclusive",
+    setup: self => {
+      self.keybind("Escape", () => App.closeWindow("quicksettings"));
+    },
     child: Widget.Box({
-      vertical: false,
+      className: "quicksettings",
+      vertical: true,
       children: [
-        Widget.Icon({
-          size: 18,
-          icon: props.icon,
+        Widget.CenterBox({
+          className: "indicators",
+          vertical: false,
+          start_widget: Widget.Box({
+            hpack: "start",
+            children: battery.available
+              ? [top_button_battery]
+              : top_buttons.slice(0, top_buttons.length / 2),
+          }),
+          end_widget: Widget.Box({
+            hpack: "end",
+            children: battery.available
+              ? top_buttons
+              : top_buttons.slice(top_buttons.length / 2),
+          }),
         }),
-        Widget.Box({
-          vertical: true,
-          vexpand: true,
-          vpack: "center",
-          children: labels,
+
+        Slider({
+          value: audio.speaker.bind("volume"),
+          icon: "audio-volume-high-symbolic",
+          min: 0,
+          max: 1,
+          on_change: ({ value }) => {
+            audio.speaker.volume = value;
+          },
         }),
+        Slider({
+          value: audio.microphone.bind("volume"),
+          icon: "audio-input-microphone-symbolic",
+          min: 0,
+          max: 1,
+          on_change: ({ value }) => {
+            audio.microphone.volume = value;
+          },
+        }),
+
+        Widget.FlowBox({
+          className: "pill-buttons",
+          setup(self) {
+            // Ensure the children have the same size.
+            self.homogeneous = true;
+            self.hexpand = true;
+            self.min_children_per_line = 2;
+            self.max_children_per_line = 2;
+            self.row_spacing = 12;
+            self.column_spacing = 12;
+            
+            self.add(
+              PillButton({
+                icon: network.wifi.bind('icon_name'),
+                label: "Wi-Fi",
+                subtext: Utils.merge(
+                  [network.wifi.bind("internet"), network.wifi.bind("ssid")],
+                  (state, ssid) => {
+                    if (state === "disconnected") {
+                      return null;
+                    }
+                    if (state === "connecting") {
+                      return "connecting...";
+                    }
+
+                    return ssid;
+                  },
+                ),
+                active: network.bind("wifi").as(wifi => wifi.enabled),
+                onToggled({ active }) {
+                  if (network.wifi.enabled == active) {
+                    return;
+                  }
+                  network.wifi.enabled = active;
+                },
+              }),
+            );
+            self.add(
+              PillButton({
+                icon: bluetooth
+                  .bind("enabled")
+                  .as(enabled => enabled ? "bluetooth-active-symbolic" : "bluetooth-disabled-symbolic"),
+                label: "Bluetooth",
+                subtext: bluetooth.bind("connected_devices").as(devices => {
+                  if (devices.length === 0) {
+                    return null;
+                  }
+                  if (devices.length === 1) {
+                    return devices[0].name;
+                  }
+
+                  return `${devices.length} devices`;
+                }),
+                active: bluetooth.bind("enabled"),
+                onToggled({ active }) {
+                  if (bluetooth.enabled == active) {
+                    return;
+                  }
+                  bluetooth.enabled = active;
+                },
+              }),
+            );
+            self.add(
+              PillButton({
+                icon: "night-light-symbolic",
+                label: "Night Light",
+                active: false,
+                onToggled(event) { },
+              }),
+            );
+          },
+        })
       ],
     }),
   });
 };
-
-export const Quicksettings = () => Widget.Window({
-  visible: false,
-  name: "quicksettings",
-  anchor: ['top', 'right'],
-  layer: "top",
-  exclusivity: "exclusive",
-  setup: self => {
-    self.keybind("Escape", () => App.closeWindow("quicksettings"));
-  },
-  child: Widget.Box({
-    className: "quicksettings",
-    vertical: true,
-    children: [
-      Widget.CenterBox({
-        className: "indicators",
-        vertical: false,
-        start_widget: Widget.Box({
-          hpack: "start",
-          children: [
-            Button({
-              icon: battery.bind("icon_name"),
-              label: battery.bind("percent").as(percent => ` ${percent}%`),
-              on_click() {
-                Utils.execAsync(`bash -c "XDG_CURRENT_DESKTOP=gnome gnome-control-center power"`);
-              },
-            })
-          ],
-        }),
-        end_widget: Widget.Box({
-          hpack: "end",
-          children: [
-            Button({
-              icon: "applets-screenshooter-symbolic",
-            }),
-            Button({
-              icon: "settings-symbolic",
-              on_click() {
-                Utils.execAsync(`bash -c "XDG_CURRENT_DESKTOP=gnome gnome-control-center"`);
-              },
-            }),
-            Button({
-              icon: "system-lock-screen-symbolic",
-            }),
-            Button({
-              icon: "system-shutdown-symbolic",
-            }),
-          ],
-        }),
-      }),
-
-      Slider({
-        value: audio.speaker.bind("volume"),
-        icon: "audio-volume-high-symbolic",
-        min: 0,
-        max: 1,
-        on_change: ({ value }) => {
-          audio.speaker.volume = value;
-        },
-      }),
-      Slider({
-        value: audio.microphone.bind("volume"),
-        icon: "audio-input-microphone-symbolic",
-        min: 0,
-        max: 1,
-        on_change: ({ value }) => {
-          audio.microphone.volume = value;
-        },
-      }),
-
-      Widget.FlowBox({
-        className: "pill-buttons",
-        setup(self) {
-          // Ensure the children have the same size.
-          self.homogeneous = true;
-          self.hexpand = true;
-          self.min_children_per_line = 2;
-          self.max_children_per_line = 2;
-          self.row_spacing = 12;
-          self.column_spacing = 12;
-          self.add(
-            PillButton({
-              icon: network.wifi.bind('icon_name'),
-              label: "Wi-Fi",
-              subtext: Utils.merge(
-                [network.wifi.bind("internet"), network.wifi.bind("ssid")],
-                (state, ssid) => {
-                  if (state === "disconnected") {
-                    return null;
-                  }
-                  if (state === "connecting") {
-                    return "connecting...";
-                  }
-
-                  return ssid;
-                },
-              ),
-              active: network.bind("wifi").as(wifi => wifi.enabled),
-              onToggled({ active }) {
-                if (network.wifi.enabled == active) {
-                  return;
-                }
-                network.wifi.enabled = active;
-              },
-            }),
-          );
-          self.add(
-            PillButton({
-              icon: bluetooth
-                .bind("enabled")
-                .as(enabled => enabled ? "bluetooth-active-symbolic" : "bluetooth-disabled-symbolic"),
-              label: "Bluetooth",
-              subtext: bluetooth.bind("connected_devices").as(devices => {
-                if (devices.length === 0) {
-                  return null;
-                }
-                if (devices.length === 1) {
-                  return devices[0].name;
-                }
-
-                return `${devices.length} devices`;
-              }),
-              active: bluetooth.bind("enabled"),
-              onToggled({ active }) {
-                if (bluetooth.enabled == active) {
-                  return;
-                }
-                bluetooth.enabled = active;
-              },
-            }),
-          );
-          self.add(
-            PillButton({
-              icon: "display-brightness-symbolic",
-              label: "Room Lights",
-              active: false,
-              onToggled(event) { },
-            }),
-          );
-          self.add(
-            PillButton({
-              icon: "night-light-symbolic",
-              label: "Night Light",
-              active: false,
-              onToggled(event) { },
-            }),
-          );
-        },
-      })
-    ],
-  }),
-});
