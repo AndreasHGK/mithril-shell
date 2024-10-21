d62adeeb97cd729d077d2e2ce3125c69770bfa8b
exellentcoin26 <60435960+exellentcoin26@users.noreply.github.com>
2024-10-21 20:55:23 +0200

    feat-wip: Struggle with css (HARD)

--- ags/timemenu/notification-list.ts
+++ ags/timemenu/notification-list.ts
@@ -1,25 +1,83 @@
 import { type Notification as NotificationInfo } from "types/service/notifications";
 
+import Notification from "./notification";
+
 const notificationService = await Service.import("notifications");
 const notifications = notificationService.bind("notifications");
 
-const Notification = (info: NotificationInfo) => {
-
-};
+const Animated = (n: NotificationInfo) => Widget.Revealer({
+  transition_duration: 200,
+  transition: "slide_down",
+  child: Notification(n),
+  setup: self => Utils.timeout(200, () => {
+    if (!self.is_destroyed)
+      self.reveal_child = true;
+  }),
+});
 
 const Placeholder = () => Widget.Box({
   visible: notifications.as(n => n.length === 0),
+  hpack: "center",
+  vpack: "center",
   children: [
     Widget.Icon("notifications-disabled-symbolic"),
     Widget.Label("Your inbox is empty"),
   ],
 });
 
-const NotificationList = () => Widget.Box({
-});
+const NotificationList = () => {
+  const map: Map<number, ReturnType<typeof Animated>> = new Map
+  let box = Widget.Box({
+    vertical: true,
+    children: notificationService.notifications.map(n => {
+      const w = Animated(n)
+      map.set(n.id, w)
+      return w
+    }),
+    visible: notifications.as(n => n.length > 0),
+  })
+
+  function remove(_: unknown, id: number) {
+    const n = map.get(id)
+    if (n) {
+      n.reveal_child = false
+      Utils.timeout(200, () => {
+        n.destroy()
+        map.delete(id)
+      })
+    }
+  }
+
+  box = box
+    .hook(notificationService, remove, "closed")
+    .hook(notificationService, (_, id: number) => {
+      if (id !== undefined) {
+        if (map.has(id))
+          remove(null, id)
+
+        const n = notificationService.getNotification(id)!
+
+        const w = Animated(n)
+        map.set(id, w)
+        box.children = [w, ...box.children]
+      }
+    }, "notified")
+
+  return Widget.Box({
+    children: [
+      Widget.Scrollable({
+        class_name: "notifications-scrollable",
+        vexpand: true,
+        hscroll: "never",
+        child: box
+      })
+    ],
+  })
+};
 
 export default () => Widget.Box({
   children: [
     Placeholder(),
+    NotificationList(),
   ],
 });
---
+++ ags/timemenu/notification.ts
@@ -0,0 +1,91 @@
+import { type Notification as NotificationInfo } from "types/service/notifications"
+
+export default (notification: NotificationInfo) => {
+  const content = Widget.Box({
+    class_name: "content",
+    children: [
+      // NotificationIcon(notification),
+      Widget.Box({
+        hexpand: true,
+        vertical: true,
+        children: [
+          Widget.Box({
+            children: [
+              Widget.Label({
+                class_name: "title",
+                xalign: 0,
+                justification: "left",
+                hexpand: true,
+                max_width_chars: 24,
+                truncate: "end",
+                wrap: true,
+                label: notification.summary.trim(),
+                use_markup: true,
+              }),
+              // Widget.Label({
+              //   class_name: "time",
+              //   vpack: "start",
+              //   label: time(notification.time),
+              // }),
+              Widget.Button({
+                class_name: "close-button",
+                vpack: "start",
+                child: Widget.Icon("window-close-symbolic"),
+                on_clicked: notification.close,
+              }),
+            ],
+          }),
+          Widget.Label({
+            class_name: "description",
+            hexpand: true,
+            use_markup: true,
+            xalign: 0,
+            justification: "left",
+            label: notification.body.trim(),
+            max_width_chars: 24,
+            wrap: true,
+          }),
+        ],
+      }),
+    ],
+  })
+
+  const actionsbox = notification.actions.length > 0 ? Widget.Revealer({
+    transition: "slide_down",
+    child: Widget.EventBox({
+      child: Widget.Box({
+        class_name: "actions horizontal",
+        children: notification.actions.map(action => Widget.Button({
+          class_name: "action-button",
+          on_clicked: () => notification.invoke(action.id),
+          hexpand: true,
+          child: Widget.Label(action.label),
+        })),
+      }),
+    }),
+  }) : null
+
+  const eventbox = Widget.EventBox({
+    vexpand: false,
+    on_primary_click: notification.dismiss,
+    on_hover() {
+      if (actionsbox)
+        actionsbox.reveal_child = true
+    },
+    on_hover_lost() {
+      if (actionsbox)
+        actionsbox.reveal_child = true
+
+      notification.dismiss()
+    },
+    child: Widget.Box({
+      vertical: true,
+      children: actionsbox ? [content, actionsbox] : [content],
+    }),
+  })
+
+  return Widget.Box({
+    class_name: `notification ${notification.urgency}`,
+    child: eventbox,
+  })
+}
