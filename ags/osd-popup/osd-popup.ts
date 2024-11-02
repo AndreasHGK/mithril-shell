372468fb64453fac6e102d3ac67627e8ba38c27e
andreashgk <andreas@hgk.me>
2024-11-02 16:49:06 +0100

    refactor: filter out duplicate values when deciding to show popup

--- ags/osd-popup/osd-popup.ts
+++ ags/osd-popup/osd-popup.ts
@@ -1,3 +1,4 @@
+import type GLib from "gi://GLib";
 import { volumeIcon } from "lib/icons";
 import type { Icon } from "lib/types";
 import { conditionalChildren } from "lib/widgets";
@@ -17,33 +18,67 @@ export const OsdPopup = (props: {
 }) =>
   Widget.Window({
     setup(self) {
-      const closeDelay = 1000;
-      // AGS maintainers please look away for the remainder of this function...
-      let cancelHide = () => {};
+      const close_delay = 1000;
 
-      self.hook(
-        props.value.emitter,
-        async (self) => {
-          cancelHide();
-          // If the volume is changed multiple times in a row the popup shouldn't disappear after
-          // the first delay, so we cancel that promise and create a new one.
-          const hidePromise = new Promise((resolve, _reject) => {
-            cancelHide = () => {
-              resolve(false);
-            };
+      // We need to keep track of the previous value as the binding may be triggered even when the
+      // value did not change.
+      let prev_value: null | number = null;
+      let hider: null | GLib.Source = null;
 
-            setTimeout(() => {
-              resolve(true);
-            }, closeDelay);
-          });
+      // Hacky way to show the window when the value changes: this abuses the fact that bindings can
+      // be transformed with a function that will run on change.
+      const value_with_listener = props.value.as((value) => {
+        // Also check for null as it would otherwise show the window when the binding is created.
+        if (value === prev_value || prev_value === null) {
+          prev_value = value;
+          return value;
+        }
+        prev_value = value;
 
-          self.set_visible(true);
-          if (await hidePromise) {
-            self.set_visible(false);
-          }
-        },
-        "changed",
-      );
+        // If the volume is changed multiple times in a row the popup shouldn't disappear after
+        // the first delay, so we cancel that promise and create a new one.
+        if (hider !== null) {
+          clearTimeout(hider);
+        }
+        hider = setTimeout(() => {
+          self.set_visible(false);
+        }, close_delay);
+
+        self.set_visible(true);
+
+        return value;
+      });
+
+      self.child = Widget.Box({
+        children: [
+          Widget.Icon({
+            icon: props.icon,
+            size: 32,
+          }),
+          Widget.CenterBox({
+            vertical: true,
+            centerWidget: Widget.Box({
+              className: "middle",
+              vertical: true,
+              children: conditionalChildren([
+                props.label
+                  ? Widget.Label({
+                      label: props.label,
+                    })
+                  : null,
+                Widget.LevelBar({
+                  vexpand: false,
+                  widthRequest: 150,
+                  heightRequest: 6,
+                  barMode: "continuous",
+                  visible: true,
+                  value: value_with_listener,
+                }),
+              ]),
+            }),
+          }),
+        ],
+      });
     },
     visible: false,
     name: `osd-popup-${props.name}`,
@@ -51,36 +86,6 @@ export const OsdPopup = (props: {
     anchor: ["bottom"],
     layer: "top",
     exclusivity: "normal",
-    child: Widget.Box({
-      children: [
-        Widget.Icon({
-          icon: props.icon,
-          size: 32,
-        }),
-        Widget.CenterBox({
-          vertical: true,
-          centerWidget: Widget.Box({
-            className: "middle",
-            vertical: true,
-            children: conditionalChildren([
-              props.label
-                ? Widget.Label({
-                    label: props.label,
-                  })
-                : null,
-              Widget.LevelBar({
-                vexpand: false,
-                widthRequest: 150,
-                heightRequest: 6,
-                barMode: "continuous",
-                visible: true,
-                value: props.value,
-              }),
-            ]),
-          }),
-        }),
-      ],
-    }),
   });
 
 /** A speaker volume on screen display popup. */
