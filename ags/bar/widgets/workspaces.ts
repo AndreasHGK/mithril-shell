ec9f304e620a1172981105eee0add0a11c69dad7
andreashgk <andreas@hgk.me>
2024-11-03 00:42:35 +0100

    refactor(bar): display active workspace animation over all intermediate workspaces

--- ags/bar/widgets/workspaces.ts
+++ ags/bar/widgets/workspaces.ts
@@ -1,3 +1,4 @@
+import type GLib from "gi://GLib";
 import { config } from "lib/settings.js";
 import type { Binding } from "types/service.js";
 import { BarWidget } from "../bar-widget.js";
@@ -27,6 +28,58 @@ export const Workspaces = (monitor: number) =>
             );
           };
 
+          // The workspace that is currently shown as active on the bar.
+          const shown_active_workspace = Variable(1);
+          // The active workspace that the bar should visually be moving towards.
+          let target_active_workspace = 1;
+          // A ticker used to smoothly interpolate the shown_active_workspace to the real active
+          // workspace.
+          let workspace_ticker: null | GLib.Source = null;
+
+          const smooth_active_workspace = Utils.merge(
+            [
+              hyprland.bind("monitors").as((monitors) => monitors[monitor].activeWorkspace.id),
+              shown_active_workspace.bind(),
+            ],
+            (real_active, shown_active) => {
+              target_active_workspace = real_active;
+
+              const step_amount = Math.abs(shown_active - real_active);
+              if (step_amount === 0) {
+                return shown_active;
+              }
+
+              if (workspace_ticker !== null) {
+                clearInterval(workspace_ticker);
+              }
+              workspace_ticker = setInterval(
+                () => {
+                  if (target_active_workspace > shown_active_workspace.value) {
+                    shown_active_workspace.value++;
+                  } else if (target_active_workspace < shown_active_workspace.value) {
+                    shown_active_workspace.value--;
+                  } else {
+                    if (workspace_ticker !== null) {
+                      clearInterval(workspace_ticker);
+                      workspace_ticker = null;
+                    }
+                  }
+                },
+                (50 + Math.min(Math.max(step_amount - 1, 0), 2) * 50) / step_amount,
+              );
+
+              // Display one step ahead of the `snown_active` variable to make the animation more
+              // responsive (play it instantly when workspace is switched).
+              let ret = shown_active;
+              if (target_active_workspace > shown_active_workspace.value) {
+                ret++;
+              } else if (target_active_workspace < shown_active_workspace.value) {
+                ret--;
+              }
+              return ret;
+            },
+          );
+
           self.hook(
             hyprland,
             (self) => {
@@ -35,12 +88,7 @@ export const Workspaces = (monitor: number) =>
 
               for (let i = old_workspace_count; i < new_workspace_count; i++) {
                 self.add(
-                  WorkspaceIndicator(
-                    hyprland
-                      .bind("monitors")
-                      .as((monitors) => monitors[monitor])
-                      .as((monitor) => monitor.activeWorkspace.id === i + 1),
-                  ),
+                  WorkspaceIndicator(smooth_active_workspace.as((active) => active === i + 1)),
                 );
               }
             },
