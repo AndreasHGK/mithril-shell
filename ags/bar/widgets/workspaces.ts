5dc2d27b8a62710cea88b7616c46b955b5bf355e
andreashgk <andreas@hgk.me>
2024-11-02 22:54:06 +0100

    refactor: improve styling and creation of workspace indicators

--- ags/bar/widgets/workspaces.ts
+++ ags/bar/widgets/workspaces.ts
@@ -1,43 +1,65 @@
 import { config } from "lib/settings.js";
+import type { Binding } from "types/service.js";
 import { BarWidget } from "../bar-widget.js";
 
 const hyprland = await Service.import("hyprland");
 
-const WorkspaceIndicator = (active = false) =>
+const WorkspaceIndicator = (active: Binding<any, any, boolean>) =>
   Widget.Box({
-    className: `workspace-indicator${active ? " active" : ""}`,
+    className: active.as((active) => `workspace-indicator${active ? " active" : ""}`),
+    vexpand: false,
+    visible: true,
   });
 
 export const Workspaces = (monitor: number) =>
   BarWidget({
-    child: Widget.Box({
-      className: "workspaces",
-      children: Utils.merge(
-        [
-          hyprland.bind("workspaces"),
-          // TODO: this breaks when unplugging a monitor that is not the last monitor in the list.
-          hyprland
-            .bind("monitors")
-            .as((monitors) => monitors[monitor].activeWorkspace),
-        ],
-        (workspaces, active) => {
-          // Workspaces start with ID 1. It is limited to 25 to keep it reasonable should hyprland
-          // return anything unexpected.
-          const workspaces_num = Math.max(
-            // Always prioritize the value from the config as a minimum amount.
-            config.minWorkspaces,
-            Math.min(25, Math.max(...workspaces.map((workspace) => workspace.id))),
+    child: Widget.CenterBox({
+      centerWidget: Widget.Box({
+        className: "workspaces",
+        setup(self) {
+          const calc_workspace_count = () => {
+            // Workspaces start with ID 1. It is limited to 25 to keep it reasonable should hyprland
+            // return anything unexpected.
+            return Math.max(
+              // Always prioritize the value from the config as a minimum amount.
+              config.minWorkspaces,
+              Math.min(25, Math.max(...hyprland.workspaces.map((workspace) => workspace.id))),
+            );
+          };
+
+          self.hook(
+            hyprland,
+            (self) => {
+              const old_workspace_count = self.children.length;
+              const new_workspace_count = calc_workspace_count();
+
+              for (let i = old_workspace_count; i < new_workspace_count; i++) {
+                self.add(
+                  WorkspaceIndicator(
+                    hyprland
+                      .bind("monitors")
+                      .as((monitors) => monitors[monitor])
+                      .as((monitor) => monitor.activeWorkspace.id === i + 1),
+                  ),
+                );
+              }
+            },
+            "workspace-added",
           );
-          const children = new Array(workspaces_num);
+          self.hook(
+            hyprland,
+            (self) => {
+              const old_workspace_count = self.children.length;
+              const new_workspace_count = calc_workspace_count();
 
-          for (let i = 0; i < workspaces_num; i++) {
-            const id = i + 1;
-            children[i] = WorkspaceIndicator(id === active.id);
-          }
-
-          return children;
+              for (let i = old_workspace_count; i > new_workspace_count; i--) {
+                self.remove(self.children[i - 1]);
+              }
+            },
+            "workspace-removed",
+          );
         },
-      ),
+      }),
     }),
     on_scroll_up: () => {
       hyprland.messageAsync("dispatch workspace +1");
