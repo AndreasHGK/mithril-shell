ce1219ef1715ba0aab1cb9c479014888f6bb5f14
exellentcoin26 <60435960+exellentcoin26@users.noreply.github.com>
2024-11-03 10:08:19 +0100

    fix(notification-menu): Inconsistent amount of notifications

--- ags/timemenu/notification-list.ts
+++ ags/timemenu/notification-list.ts
@@ -1,11 +1,14 @@
+import type { Variable as VariableType } from "types/variable";
+
 import Notification from "./notification";
 
 const notificationService = await Service.import("notifications");
-const notifications = notificationService.bind("notifications");
 
-const Placeholder = () =>
+type NotificationMap = VariableType<Map<number, ReturnType<typeof Notification>>>;
+
+const Placeholder = (notifications: NotificationMap) =>
   Widget.Box({
-    visible: notifications.as((n) => n.length === 0),
+    visible: notifications.bind().as((m) => m.size === 0),
     className: "placeholder",
     hexpand: true,
     vexpand: true,
@@ -14,42 +17,62 @@ const Placeholder = () =>
     children: [Widget.Icon("notifications-disabled-symbolic"), Widget.Label("Your inbox is empty")],
   });
 
-const NotificationList = () => {
-  // Keep track of registered notifications.
-  const map: Map<number, ReturnType<typeof Notification>> = new Map();
+const NotificationList = (notifications: NotificationMap) => {
+  const notify = (self: ReturnType<typeof Widget.Box>, id: number | undefined) => {
+    if (id === undefined) return;
+
+    const info = notificationService.getNotification(id);
+    if (info === undefined) return;
+
+    const notification = Notification(info);
+    notifications.setValue(notifications.getValue().set(id, notification));
+    self.children = [notification, ...self.children];
+  };
+
+  const remove = (id: number | undefined) => {
+    if (id === undefined) return;
+
+    const m = notifications.getValue();
+    const notification = m.get(id);
+    if (notification === undefined) {
+      return;
+    }
+
+    m.delete(id);
+    notification.destroy();
+    notifications.setValue(m);
+  };
+
   return Widget.Scrollable({
     className: "scrollable",
-    visible: notifications.as((n) => n.length > 0),
+    visible: notifications.bind().as((n) => n.size > 0),
     vexpand: true,
     hscroll: "never",
     child: Widget.Box({
       vertical: true,
       children: notificationService.notifications.map((info) => {
         const notification = Notification(info);
-        map.set(info.id, notification);
+        notifications.setValue(notifications.getValue().set(info.id, notification));
         return notification;
       }),
       setup(self) {
         self
-          .hook(notificationService, (_, id: number) => map.delete(id), "closed")
           .hook(
             notificationService,
-            (_, id: number) => {
-              const info = notificationService.getNotification(id);
-              if (info === undefined) return;
-              const notification = Notification(info);
-              map.set(id, notification);
-              self.children = [notification, ...self.children];
-            },
+            (_: unknown, id: number | undefined) => notify(self, id),
             "notified",
-          );
-      }
+          )
+          .hook(notificationService, (_: unknown, id: number | undefined) => remove(id), "closed");
+      },
     }),
   });
 };
 
-export default () =>
-  Widget.Box({
+export default () => {
+  // Keep track of registered notifications.
+  const notifications: NotificationMap = Variable(new Map());
+  return Widget.Box({
     className: "list",
-    children: [Placeholder(), NotificationList()],
+    children: [Placeholder(notifications), NotificationList(notifications)],
   });
+};
