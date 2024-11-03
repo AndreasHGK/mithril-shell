a81e6df6918fd7d4a7938a72dd3b7c4f54097ae5
exellentcoin26 <60435960+exellentcoin26@users.noreply.github.com>
2024-11-03 09:14:00 +0100

    refactor: Move setup functionality to `Box::setup`

--- ags/timemenu/notification-list.ts
+++ ags/timemenu/notification-list.ts
@@ -7,42 +7,44 @@ const Placeholder = () =>
   Widget.Box({
     visible: notifications.as((n) => n.length === 0),
     className: "placeholder",
+    hexpand: true,
+    vexpand: true,
     hpack: "center",
     vpack: "center",
     children: [Widget.Icon("notifications-disabled-symbolic"), Widget.Label("Your inbox is empty")],
   });
 
 const NotificationList = () => {
+  // Keep track of registered notifications.
   const map: Map<number, ReturnType<typeof Notification>> = new Map();
-  let listBox = Widget.Box({
-    vertical: true,
-    children: notificationService.notifications.map((info) => {
-      const notification = Notification(info);
-      map.set(info.id, notification);
-      return notification;
-    }),
-  });
-
-  listBox = listBox
-    .hook(notificationService, (_, id: number) => map.delete(id), "closed")
-    .hook(
-      notificationService,
-      (_, id: number) => {
-        const info = notificationService.getNotification(id);
-        if (info === undefined) return;
-        const notification = Notification(info);
-        map.set(id, notification);
-        listBox.children = [notification, ...listBox.children];
-      },
-      "notified",
-    );
-
   return Widget.Scrollable({
     className: "scrollable",
     visible: notifications.as((n) => n.length > 0),
     vexpand: true,
     hscroll: "never",
-    child: listBox,
+    child: Widget.Box({
+      vertical: true,
+      children: notificationService.notifications.map((info) => {
+        const notification = Notification(info);
+        map.set(info.id, notification);
+        return notification;
+      }),
+      setup(self) {
+        self
+          .hook(notificationService, (_, id: number) => map.delete(id), "closed")
+          .hook(
+            notificationService,
+            (_, id: number) => {
+              const info = notificationService.getNotification(id);
+              if (info === undefined) return;
+              const notification = Notification(info);
+              map.set(id, notification);
+              self.children = [notification, ...self.children];
+            },
+            "notified",
+          );
+      }
+    }),
   });
 };
 
